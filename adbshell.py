98c9ca3824ee13ac2baae3c08f85a46e16e5a125
Aznon <aznone99@gmail.com>
2022-11-01 02:16:59 +0800

    提交

--- adbshell.py
+++ adbshell.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 
 #查看链接
 #查看包名和启动项Activity
@@ -9,4 +9,14 @@ import os
 
 ADBhead='adb '
 ADBshellhead='adb shell '
-ADBscreenshot=AdbShellHead+'/system/bin/screencap -p '+Filepath
+ADBscreenshot=' /system/bin/screencap -p '
+#截图
+# def ADBscreenshot(devicesName,apkname):
+    # path="log/"+apkname
+    #如果没有路径就创建路径
+    #截图放进目标文件夹 保存文件名
+
+    # if not os.path.exists(path):
+    #     os.makedirs(path)
+    # cmd=ADBshellhead+"-s "+devicesName+ADBscreenshot+path
+    # os.system(cmd)
